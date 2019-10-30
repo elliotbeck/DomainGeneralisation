55fb5a5c176e64153bb4fae6498b4380e3dc98c8
elliotbeck <elliot.beck@hotmail.com>
2019-10-30 10:29:13 +0100

    bug fix

--- DAOT/models/DAOT.py
+++ DAOT/models/DAOT.py
@@ -86,13 +86,13 @@ class critic(tf.keras.Model):
                                                         weights=resnet_weights, input_shape=in_shape),
             tf.keras.layers.Flatten(),
             tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Dense(1028, activation=tf.keras.layers.LeakyReLU),
+            tf.keras.layers.Dense(1028, activation=tf.keras.layers.LeakyReLU()),
             tf.keras.layers.Dropout(0.5),
             tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Dense(1028, activation=tf.keras.layers.LeakyReLU),
+            tf.keras.layers.Dense(1028, activation=tf.keras.layers.LeakyReLU()),
             tf.keras.layers.Dropout(0.5),
             tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Dense(34, activation=tf.keras.layers.LeakyReLU)
+            tf.keras.layers.Dense(34, activation=tf.keras.layers.LeakyReLU())
             #tf.keras.layers.Dropout(0.5),
             #tf.keras.layers.BatchNormalization(),
             #tf.keras.layers.Dense(34, activation='relu')
