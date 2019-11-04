20d57d15e46bf9ff9324bbcf2eea32a13125cb5a
elliotbeck <elliot.beck@hotmail.com>
2019-11-04 16:30:42 +0100

    debug

--- DAOT/util.py
+++ DAOT/util.py
@@ -66,7 +66,7 @@ def compute_optimal_transport(M, r, c, lam=10, epsilon=1e-3):
         u = tf.math.reduce_sum(P, axis=1)
         P *= tf.reshape(r/u,[-1, 1])
         P *= tf.reshape(c/tf.math.reduce_sum(P, axis=0),[1, -1])
-        print(P)
+    print(P)
     return P, tf.math.reduce_sum(P * M)
     
     
