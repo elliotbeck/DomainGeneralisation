e531b3b15e5cef141f0be8d92f3b236803f30560
elliotbeck <elliot.beck@hotmail.com>
2019-11-06 15:21:43 +0100

    image function implemented again

--- DAOT/main.py
+++ DAOT/main.py
@@ -29,7 +29,7 @@ import experiment_repo as repo
 import util
 import local_settings
 
-DEBUG = True
+DEBUG = False
 
 parser = argparse.ArgumentParser(description='Train my model.')
 parser.add_argument('--config', type=str, 
@@ -183,10 +183,9 @@ def loss_fn_critic(model_critic, model_generator, features1, features2, config, 
     label_generated2 = label2
 
     X_generated1 = model_generator(inputs1, training=training)
-    print(X_generated1[0])  
-    #plt.imsave('/cluster/home/ebeck/DomainGeneralisation/DAOT/images/fake.png', X_generated1[0])
-    #plt.imsave('/cluster/home/ebeck/DomainGeneralisation/DAOT/images/original.png', inputs1[0])
-    #plt.imsave('/cluster/home/ebeck/DomainGeneralisation/DAOT/images/peturbation.png', X_generated1[0]-inputs1[0])
+    plt.imsave('/cluster/home/ebeck/DomainGeneralisation/DAOT/images/fake.png', X_generated1[0])
+    plt.imsave('/cluster/home/ebeck/DomainGeneralisation/DAOT/images/original.png', inputs1[0])
+    plt.imsave('/cluster/home/ebeck/DomainGeneralisation/DAOT/images/peturbation.png', X_generated1[0]-inputs1[0])
     X_generated2 = model_generator(inputs2, training=training)
     X_critic_true1 = model_critic(inputs1, training=training)
     X_critic_true2 = model_critic(inputs2, training=training)
