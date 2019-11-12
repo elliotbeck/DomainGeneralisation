4c344bce7dc684aacd2134c3fb36dbbbbea81be0
elliotbeck <elliot.beck@hotmail.com>
2019-11-12 20:54:47 +0100

    add 3 layer config

--- DAOT_mnist/models/resnet.py
+++ DAOT_mnist/models/resnet.py
@@ -8,7 +8,7 @@ class ResNet50(tf.keras.Model):
         super().__init__(*args, **kwargs)
         self.config = config
 
-        in_shape = self.input_shape + [1]
+        in_shape = self.input_shape + [3]
 
         self.model = tf.keras.models.Sequential([
             tf.keras.applications.resnet50.ResNet50(include_top=False,
@@ -23,7 +23,7 @@ class ResNet50(tf.keras.Model):
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Dense(num_classes, activation='softmax')
         ])
-        self.model.build([None] + self.input_shape) + [1] # Batch input shape.
+        self.model.build([None] + self.input_shape) + [3] # Batch input shape.
 
     def call(self, inputs, training=None, mask=None):
         return self.model(inputs, training, mask)
