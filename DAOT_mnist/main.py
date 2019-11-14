8132023bdd53a2177355f95bcae15aed42eff9ce
elliotbeck <elliot.beck@hotmail.com>
2019-11-14 15:10:23 +0100

    typo

--- DAOT_mnist/main.py
+++ DAOT_mnist/main.py
@@ -148,7 +148,7 @@ def _preprocess_exampe(model, example, dataset_name, e):
     print(colors.shape)
     # Apply the color to the image by zeroing out the other color channel
     images = tf.stack([example["image"], example["image"]], axis=1)
-    images[colors_re] *= 0
+    images[1,colors_re,:,:] *= 0
     images = tf.stack(images)
     example['image'] = images
     example["image"] = tf.cast(example["image"], dtype=tf.float32)/255.
