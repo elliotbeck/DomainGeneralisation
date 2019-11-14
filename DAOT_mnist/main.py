60b234bd045043bf9959d741366db183115e2331
elliotbeck <elliot.beck@hotmail.com>
2019-11-14 17:30:27 +0100

    test

--- DAOT_mnist/main.py
+++ DAOT_mnist/main.py
@@ -193,6 +193,7 @@ def _preprocess_exampe(model, example, dataset_name, e):
     # Assign a color based on the label; flip the color with probability e
     colors = tf_xor(labels, tf_bernoulli(e, 1))
     re_colors = 1-colors
+    re_colors = tf.cast(re_colors, dtype=tf.int32)
     # Apply the color to the image by zeroing out the other color channel
     images = tf.stack([example["image"], example["image"]], axis=1)
     print(images[0,0,:,:].shape)
