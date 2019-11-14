292e3bc502405839404660f5c16ee07e567f9b1c
elliotbeck <elliot.beck@hotmail.com>
2019-11-14 12:21:16 +0100

    use numpy

--- DAOT_mnist/main.py
+++ DAOT_mnist/main.py
@@ -134,7 +134,7 @@ def _preprocess_exampe(model, example, dataset_name, e):
     def tf_bernoulli(p, size):
       return tf.cast([tf.random.uniform([size]) < p], dtype=tf.float32)
     def tf_xor(a, b):
-      return tf.abs((a-b)) # Assumes both inputs are either 0 or 1
+      return np.absolute((a-b)) # Assumes both inputs are either 0 or 1
     # 2x subsample for computational convenience
     example["image"] = tf.reshape(example["image"],[-1, 28, 28])[:, ::2, ::2]
     # Assign a binary label based on the digit; flip label with probability 0.25
