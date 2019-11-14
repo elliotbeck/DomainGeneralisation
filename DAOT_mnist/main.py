2948c9921266a1b397fbe5b1d28b3bd92945f8f6
elliotbeck <elliot.beck@hotmail.com>
2019-11-14 18:11:51 +0100

    test

--- DAOT_mnist/main.py
+++ DAOT_mnist/main.py
@@ -198,8 +198,8 @@ def _preprocess_exampe(model, example, dataset_name, e):
     print(re_colors)
     # Apply the color to the image by zeroing out the other color channel
     images = tf.stack([example["image"], example["image"]], axis=1)
-    images = tf.unstack(images, axis = 1)
-    print(images.shape)
+    images = tf.unstack(images, axis = 0)
+    print(images[0].shape)
     images[0][re_colors] = [np.zeros((14,14))]
     print(images[0][re_colors].shape)
     images = tf.stack(images)
