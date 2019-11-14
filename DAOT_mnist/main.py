e301690587927a0df94029bde838aeb15058e65b
elliotbeck <elliot.beck@hotmail.com>
2019-11-14 14:49:44 +0100

    test

--- DAOT_mnist/main.py
+++ DAOT_mnist/main.py
@@ -148,7 +148,8 @@ def _preprocess_exampe(model, example, dataset_name, e):
     images = tf.stack([example["image"], example["image"]], axis=1)
     images = tf.unstack(images)
     colors = tf.unstack(colors)
-    images[(1-colors)] *= 0
+    colors_re = 1-colors
+    images[colors_re] *= 0
     images = tf.stack(images)
     example['image'] = images
     example["image"] = tf.cast(example["image"], dtype=tf.float32)/255.
