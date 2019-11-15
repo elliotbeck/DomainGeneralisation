8227167287192d26612bf86162acee7edb0f97e4
elliotbeck <elliot.beck@hotmail.com>
2019-11-15 14:54:55 +0100

    changed dtype

--- DAOT_mnist/main.py
+++ DAOT_mnist/main.py
@@ -68,7 +68,7 @@ def loss_fn(model, features, config, training):
     model_output = tf.cast(model_output, dtype = tf.float32)
     print(model_output)
     classification_loss = tf.losses.binary_crossentropy(
-        tf.one_hot(label, axis=-1, depth=config.num_classes),
+        tf.one_hot(label, axis=-1, depth=config.num_classes, dtype = tf.int32),
         model_output, from_logits=False)
     mean_classification_loss = tf.reduce_mean(classification_loss) 
 
