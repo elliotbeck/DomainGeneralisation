f4222f0470170119f8f155e24af1d9db9ef0bc82
elliotbeck <elliot.beck@hotmail.com>
2019-11-15 09:03:39 +0100

    test

--- DAOT_mnist/main.py
+++ DAOT_mnist/main.py
@@ -200,7 +200,7 @@ def _preprocess_exampe(model, example, dataset_name, e):
     images = tf.stack([example["image"], example["image"]], axis=1)
     #images[0,re_colors,:,:] *= tf.constant(0)
     images = np.array(images)
-    images[0,re_colors,:,:] = 0
+    images[0,re_colors,:,:].assign(tf.constant(np.zeros((14,14))))
     #images[0,re_colors,:,:] = tf.math.scalar_mul(tf.constant(0, dtype=tf.float32), images[0,re_colors,:,:])
     #images[0,re_colors,:,:] = np.zeros((14,14))
     #K.set_value(images[0,re_colors,:,:], np.zeros((14,14)))
