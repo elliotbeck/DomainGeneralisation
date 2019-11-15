dad7d418ba8284692c70b2885c69dd39584bf004
elliotbeck <elliot.beck@hotmail.com>
2019-11-15 18:51:05 +0100

    removed last print command

--- DAOT_mnist_test/models/DAOT_mnist.py
+++ DAOT_mnist_test/models/DAOT_mnist.py
@@ -64,7 +64,6 @@ class generator(tf.keras.Model):
 
     def call(self, inputs, training=None, mask=None):
         X_shortcut = inputs
-        print(self.model(inputs, training, mask))
         output = tf.keras.layers.add([config_dic2["lambd"]*self.model(inputs, training, mask), X_shortcut])
         #output = tf.keras.activations.tanh(output)
         return output
