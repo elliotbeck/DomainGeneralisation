868f378be15070442a17bba6a06ee4c42b496dd7
elliotbeck <elliot.beck@hotmail.com>
2019-11-15 14:34:49 +0100

    test

--- DAOT_mnist/models/resnet.py
+++ DAOT_mnist/models/resnet.py
@@ -8,7 +8,7 @@ class ResNet50(tf.keras.Model):
         super().__init__(*args, **kwargs)
         self.config = config
 
-        in_shape = [1] + self.input_shape 
+        in_shape = [2] + self.input_shape 
 
         self.model = tf.keras.models.Sequential([
             tf.keras.layers.Flatten(),
@@ -23,7 +23,7 @@ class ResNet50(tf.keras.Model):
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Dense(num_classes, activation='softmax')
         ])
-        self.model.build([None] + [1] + self.input_shape ) # Batch input shape.
+        self.model.build([None] + [2] + self.input_shape ) # Batch input shape.
 
     def call(self, inputs, training=None, mask=None):
         return self.model(inputs, training, mask)
