dd741d51a1619706079ebc98eaa4a80ea3c87859
elliotbeck <elliot.beck@hotmail.com>
2019-11-20 13:31:00 +0100

    test

--- InvariantRiskMinimization_PACS/main.py
+++ InvariantRiskMinimization_PACS/main.py
@@ -158,7 +158,7 @@ for restart in range(flags.n_restarts):
   for step in range(flags.steps):
     for env in envs:
       logits = mlp(env['images'])
-      logits = logits.squeeze(1).float()
+      logits = logits.squeeze().float()
       env['labels'] = env['labels'].squeeze()
       env['nll'] = mean_nll(logits, env['labels'])
       env['acc'] = mean_accuracy(logits, env['labels'])
