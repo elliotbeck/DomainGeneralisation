9f6ca47871629a7351850d05037914317dce6c29
elliotbeck <elliot.beck@hotmail.com>
2019-11-25 09:58:04 +0100

    change config path

--- DAOT_mnist/main.py
+++ DAOT_mnist/main.py
@@ -34,7 +34,7 @@ random.seed(DAOT_mnist.config_seed)
 
 parser = argparse.ArgumentParser(description='Train my model.')
 parser.add_argument('--config', type=str, 
-    default="configs/config_class.json",
+    default="configs/config_class_daot_mnist.json",
     help='Path to config file.')
 parser.add_argument('--all_checkpoints_folder', type=str, 
     default="checkpoints_pretr", help='Checkpoint folder name.')
