1cc9a613f57468fa0b8eb34fae93b724e9217b66
elliotbeck <elliot.beck@hotmail.com>
2019-11-25 09:45:02 +0100

    deleted densenet model

--- DAOT_mnist/models/__init__.py
+++ DAOT_mnist/models/__init__.py
@@ -1,12 +1,9 @@
-from models.densenet import DenseNet
 from models.DAOT_mnist import basic_nn
 from models.DAOT_mnist import generator
 from models.DAOT_mnist import critic
 
 def get_model(name, config):
-    if name == "densenet121":
-        return DenseNet(config.num_classes, config.densenet_weights, config)
-    elif name == "basic_nn":
+    if name == "basic_nn":
         return basic_nn(config.num_classes, config)
     elif name == "critic":
         return critic(config.num_classes, config.resnet_weights, config)
