3c6833501c9814ebf10b4a8c1b4bc00ca0946f02
elliotbeck <elliot.beck@hotmail.com>
2019-12-06 17:13:12 +0100

    removed typo

--- CrossGrad/models/CrossGrad.py
+++ CrossGrad/models/CrossGrad.py
@@ -16,7 +16,7 @@ class model_domain(tf.keras.Model):
         for layer in self.model.layers:
             layer.trainable = False 
         self.model.add(tf.keras.layers.GlobalAveragePooling2D())                                    
-        self.model.add(tf.keras.layers.Flatten(input_shape=in_shape))
+        self.model.add(tf.keras.layers.Flatten())
         self.model.add(tf.keras.layers.BatchNormalization())
         self.model.add(tf.keras.layers.Dense(256, activation='relu'))
         self.model.add(tf.keras.layers.Dropout(0.5))
@@ -50,7 +50,7 @@ class model_label(tf.keras.Model):
         for layer in self.model.layers:
             layer.trainable = False         
         self.model.add(tf.keras.layers.GlobalAveragePooling2D())                                    
-        self.model.add(tf.keras.layers.Flatten(input_shape=in_shape))
+        self.model.add(tf.keras.layers.Flatten())
         self.model.add(tf.keras.layers.BatchNormalization())
         self.model.add(tf.keras.layers.Dense(256, activation='relu'))
         self.model.add(tf.keras.layers.Dropout(0.5))
