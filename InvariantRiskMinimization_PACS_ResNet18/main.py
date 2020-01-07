255fa06c5a3dba352c38d62d080b04c61dcd772e
elliotbeck <elliot.beck@hotmail.com>
2020-01-07 17:00:52 +0100

    change architecture

--- InvariantRiskMinimization_PACS_ResNet18/main.py
+++ InvariantRiskMinimization_PACS_ResNet18/main.py
@@ -117,14 +117,16 @@ for restart in range(flags.n_restarts):
         lin1 = nn.Linear(14 * 14, flags.hidden_dim)
       else:
           resnet = models.resnet50(pretrained=True)
-          in_ftr  = resnet.fc.in_features
+          resnet.fc = torch.nn.Sequential(
+                torch.nn.Linear(
+                in_features=2048,
+                    out_features=1))
           # ct = 0
           # for child in resnet.children():
           #   ct += 1
           #   if ct < 10:
           #     for param in child.parameters():
           #       param.requires_grad = False
-      resnet.fc = nn.Linear(in_ftr, flags.hidden_dim)
       lin1 = nn.Linear(flags.hidden_dim, flags.hidden_dim)
       lin2 = nn.Linear(flags.hidden_dim, flags.hidden_dim)
       lin3 = nn.Linear(flags.hidden_dim, 7)
