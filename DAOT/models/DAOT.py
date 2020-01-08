294295a9046663568ff93fe7000ba9ab54e147f4
elliotbeck <elliot.beck@hotmail.com>
2020-01-08 09:56:11 +0100

    reduce layers

--- DAOT/models/DAOT.py
+++ DAOT/models/DAOT.py
@@ -20,6 +20,7 @@ class ResNet50(tf.keras.Model):
         self.model = tf.keras.Sequential([
             tf.compat.v1.keras.applications.ResNet50(include_top=False,
                                                         weights=resnet_weights, input_shape=in_shape),
+            tf.keras.layers.GlobalAveragePooling2D(),
             tf.keras.layers.Flatten(),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Dense(2048, activation='relu'),
@@ -86,6 +87,7 @@ class critic(tf.keras.Model):
         self.model = tf.keras.Sequential([
             tf.compat.v1.keras.applications.ResNet50(include_top=False,
                                                         weights=resnet_weights, input_shape=in_shape),
+            tf.keras.layers.GlobalAveragePooling2D(),
             tf.keras.layers.Flatten(),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Dense(2048, activation='relu'),
