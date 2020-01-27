8b3dce94ca542c67dbeaea11a07644294dfdafcf
elliotbeck <elliot.beck@hotmail.com>
2020-01-27 12:28:03 +0100

    simplify architecture

--- CrossGrad/models/CrossGrad.py
+++ CrossGrad/models/CrossGrad.py
@@ -22,9 +22,6 @@ class model_domain(tf.keras.Model):
         self.model.add(tf.keras.layers.Dense(1024, activation='relu'))
         self.model.add(tf.keras.layers.Dropout(0.5))
         self.model.add(tf.keras.layers.BatchNormalization())
-        self.model.add(tf.keras.layers.Dense(256, activation='relu'))
-        self.model.add(tf.keras.layers.Dropout(0.5))
-        self.model.add(tf.keras.layers.BatchNormalization())
         self.model.add(tf.keras.layers.Dense(num_classes_domain))
         self.model.build([None] + self.input_shape + [3])  # Batch input shape.
 
@@ -55,9 +52,6 @@ class model_label(tf.keras.Model):
         self.model.add(tf.keras.layers.Dropout(0.5))
         self.model.add(tf.keras.layers.BatchNormalization())
         self.model.add(tf.keras.layers.Dense(1024, activation='relu'))
-        self.model.add(tf.keras.layers.Dropout(0.5))
-        self.model.add(tf.keras.layers.BatchNormalization())
-        self.model.add(tf.keras.layers.Dense(256, activation='relu'))
         self.model.add(tf.keras.layers.Dropout(0.5))
         self.model.add(tf.keras.layers.BatchNormalization())
         self.model.add(tf.keras.layers.Dense(num_classes_label))
