693a73f77a0bac0205018f441b82c303c73ccf5d
elliotbeck <elliot.beck@hotmail.com>
2020-01-27 10:36:56 +0100

    append picture

--- DAOT_mnist/main.py
+++ DAOT_mnist/main.py
@@ -154,8 +154,10 @@ def loss_fn_critic(model_critic, model_generator, features1, features2, config, 
     label_generated2 = label2
 
     X_generated1 = model_generator(inputs1, training=training)
-    print(X_generated1[0].shape)
-    plt.imsave('/cluster/home/ebeck/DomainGeneralisation/DAOT_mnist/images/fake.png', X_generated1[0])
+    image_test = tf.stack(X_generated1[0], tf.zeros([14,14], 
+        dtype=tf.float64), axis=-1)
+    print(image_test.shape)
+    plt.imsave('/cluster/home/ebeck/DomainGeneralisation/DAOT_mnist/images/fake.png', image_test)
     plt.imsave('/cluster/home/ebeck/DomainGeneralisation/DAOT_mnist/images/original.png', inputs1[0])
     plt.imsave('/cluster/home/ebeck/DomainGeneralisation/DAOT_mnist/images/peturbation.png', X_generated1[0]-inputs1[0])
     X_generated2 = model_generator(inputs2, training=training)
