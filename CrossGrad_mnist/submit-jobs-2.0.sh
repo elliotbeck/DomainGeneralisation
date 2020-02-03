feaf14ae87b5ab699a55d01504c726abcd5d18f2
elliotbeck <elliot.beck@hotmail.com>
2020-02-03 19:32:47 +0100

    adjust bash scripts

--- CrossGrad_mnist/submit-jobs-2.0.sh
+++ CrossGrad_mnist/submit-jobs-2.0.sh
@@ -15,24 +15,20 @@ module load eth_proxy python_gpu/3.6.4
 module load cuda/10.0.130
 module load cudnn/7.6.4
 
-
-export CONFIG="/cluster/home/ebeck/ResNet50/configs/config_class_resnet.json"
-
-export DECAY_EVERY=5000
-export NUM_EPOCHS=40
-export BATCH_SIZE=32
-
-
-for VAR_LEARN_RATE in 0.01 0.001 
+for VAR_BATCH_SIZE in 16 32 64 
 do
-    export LEARN_RATE=$VAR_LEARN_RATE
-    for  VAR_DO_RATE in 0.1 0.3 0.5 0.7 
+    export BATCH_SIZE=$VAR_BATCH_SIZE
+    for  VAR_EPSD in 0.5 1 2 2.5
     do
-        export DO_RATE=$VAR_DO_RATE
-        for  VAR_L2_PEN in 0.01 0.001 0.0001 0.00001
+        export EPSD=$VAR_EPSD
+        for  VAR_EPSL in 0.5 1 2 2.5
         do
-            export L2_PEN=$VAR_L2_PEN 
-            sh submit-train-2.0.sh
+            export EPSL=$VAR_EPSL
+            for  VAR_ALPHA in 0.1 0.25 0.5 0.75 0.9
+            do
+                export ALPHA=$VAR_ALPHA
+                sh submit-train-2.0.sh
+            done 
         done
     done    
 done
