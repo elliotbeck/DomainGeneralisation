c370b0b986a4eaf0a016bd74a58a840cf1f51588
elliotbeck <elliot.beck@hotmail.com>
2020-02-03 18:26:14 +0100

    remove redundant softmax

--- InvariantRiskMinimization_MNIST_tf/main.py
+++ InvariantRiskMinimization_MNIST_tf/main.py
@@ -47,8 +47,7 @@ class MLP(tf.keras.Model):
                                     kernel_initializer = tf.initializers.GlorotUniform()),
             tf.keras.layers.Dense(flags.hidden_dim, activation='relu', 
                                     kernel_initializer = tf.initializers.GlorotUniform()),
-            tf.keras.layers.Dense(num_classes, activation='softmax', 
-                                    kernel_initializer = tf.initializers.GlorotUniform())
+            tf.keras.layers.Dense(num_classes, kernel_initializer = tf.initializers.GlorotUniform())
         ])
         self.model.build([None] + self.input_shape + [2])  # Batch input shape.
 
