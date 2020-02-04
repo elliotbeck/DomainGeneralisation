afdef6baf79beac2d543e884ea654bf76b18800d
elliotbeck <elliot.beck@hotmail.com>
2020-02-04 09:32:20 +0100

    change job name

--- CrossGrad_mnist/submit-train-2.0.sh
+++ CrossGrad_mnist/submit-train-2.0.sh
@@ -2,7 +2,6 @@
 
 bsub -J $JOB_NAME -n 1 -W $ttime -R "rusage[mem=$mem_per_gpu, ngpus_excl_p=$num_gpu_per_job]" -R "select[gpu_model1==GeForceGTX1080Ti]" \
 python -W ignore main.py \
---batch_size ${BATCH_SIZE} \
 --epsD ${EPSD} \
 --epsL ${EPSL} \
 --alpha ${ALPHA} \
