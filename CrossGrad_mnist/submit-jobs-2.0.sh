b12abd55948285d4fd062d7a492c24fdf19d3994
elliotbeck <elliot.beck@hotmail.com>
2020-02-04 09:29:38 +0100

    rrmove batch_size from grid

--- CrossGrad_mnist/submit-jobs-2.0.sh
+++ CrossGrad_mnist/submit-jobs-2.0.sh
@@ -15,20 +15,19 @@ module load eth_proxy python_gpu/3.6.4
 module load cuda/10.0.130
 module load cudnn/7.6.4
 
-for VAR_BATCH_SIZE in 16 32 64 
+
+export BATCH_SIZE=$VAR_BATCH_SIZE
+for  VAR_EPSD in 0.5 1 2 2.5
 do
-    export BATCH_SIZE=$VAR_BATCH_SIZE
-    for  VAR_EPSD in 0.5 1 2 2.5
+    export EPSD=$VAR_EPSD
+    for  VAR_EPSL in 0.5 1 2 2.5
     do
-        export EPSD=$VAR_EPSD
-        for  VAR_EPSL in 0.5 1 2 2.5
+        export EPSL=$VAR_EPSL
+        for  VAR_ALPHA in 0.1 0.25 0.5 0.75 0.9
         do
-            export EPSL=$VAR_EPSL
-            for  VAR_ALPHA in 0.1 0.25 0.5 0.75 0.9
-            do
-                export ALPHA=$VAR_ALPHA
-                sh submit-train-2.0.sh
-            done 
-        done
-    done    
-done
+            export ALPHA=$VAR_ALPHA
+            sh submit-train-2.0.sh
+        done 
+    done
+done    
+
