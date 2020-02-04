12aeb5bcc2a8c3cca945477f82e087629796bf4f
elliotbeck <elliot.beck@hotmail.com>
2020-02-04 09:16:59 +0100

    ignore warnings

--- CrossGrad_mnist/submit-train-2.0.sh
+++ CrossGrad_mnist/submit-train-2.0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 bsub -J $JOB_NAME -n 1 -W $ttime -R "rusage[mem=$mem_per_gpu, ngpus_excl_p=$num_gpu_per_job]" -R "select[gpu_model1==GeForceGTX1080Ti]" \
-python main.py \
+python -W ignore main.py \
 --batch_size ${BATCH_SIZE} \
 --epsD ${EPSD} \
 --epsL ${EPSL} \
