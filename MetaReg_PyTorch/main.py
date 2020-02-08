f23eac0b933492b50cf50af4c91b3225e2e6fde4
elliotbeck <elliot.beck@hotmail.com>
2020-02-08 16:42:09 +0100

    change test domain

--- MetaReg_PyTorch/main.py
+++ MetaReg_PyTorch/main.py
@@ -28,7 +28,7 @@ for k,v in sorted(vars(flags).items()):
 random.seed(flags.seed)
 
 # load data
-dataset1 = HDF5Dataset('/cluster/work/math/ebeck/data/pacs/cartoon_train.hdf5')
+dataset1 = HDF5Dataset('/cluster/work/math/ebeck/data/pacs/art_painting_train.hdf5')
 train_data1 = data.DataLoader(dataset1, num_workers=1, batch_size=flags.batch_size, 
                               shuffle=True, drop_last=True)
 dataset2 = HDF5Dataset('/cluster/work/math/ebeck/data/pacs/sketch_train.hdf5')
@@ -40,7 +40,7 @@ train_data3 = data.DataLoader(dataset3, num_workers=1, batch_size=flags.batch_si
 train_data_full = data.DataLoader(data.ConcatDataset([dataset1, dataset2, dataset3]), 
                                                     num_workers=1, batch_size=flags.batch_size, 
                                                     shuffle=True, drop_last=True)
-dataset = HDF5Dataset('/cluster/work/math/ebeck/data/pacs/art_painting_test.hdf5')
+dataset = HDF5Dataset('/cluster/work/math/ebeck/data/pacs/cartoon_test.hdf5')
 test_data = data.DataLoader(dataset, num_workers=1, batch_size=flags.batch_size, 
                               shuffle=True, drop_last=True)
 
